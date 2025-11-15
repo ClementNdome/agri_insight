7804a272deae3fe0b44a109a3e473b8ab276778a
ClementNdome <clementndome20@gmail.com>
2025-11-15 07:22:50 +0300

    buid

--- build.sh
+++ build.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 # build.sh
 
-echo "Installing dependencies..."
+# Upgrade pip
+pip install --upgrade pip
+
+# Install setuptools and wheel first
+pip install setuptools==69.0.2 wheel==0.42.0
+
+# Then install the rest of requirements
 pip install -r requirements.txt
 
-echo "Collecting static files..."
+# Collect static files
 python manage.py collectstatic --noinput
 
-echo "Running migrations..."
+# Run migrations
 python manage.py migrate
